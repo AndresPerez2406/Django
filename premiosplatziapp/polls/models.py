09e38a0320c1427d364a38dedaf927eeffb011e6
Andres Figueredo <afelipefigueredo@gmail.com>
2023-03-31 11:04:04 -0500

    Metodo get, filter, create, crear superusuario y entrar a interfaz admin, dandole acceso a question

--- premiosplatziapp/polls/models.py
+++ premiosplatziapp/polls/models.py
@@ -4,7 +4,7 @@ import datetime
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateField("date published")
+    pub_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.question_text
@@ -13,7 +13,7 @@ class Question(models.Model):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
-class Choise(models.Model):
+class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
